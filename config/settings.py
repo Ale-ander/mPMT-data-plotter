adc319bae06c2df00f40ee62d3d0f5d43965d496
Ale-ander <alexanderdinola@gmail.com>
2026-05-05 16:16:48 +0200

    Fixed Autofield warning

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--)=6aw6n%)p8l6z590!s&&8sghn@$45m8^73fu0$afm=hym0z=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.16.11.194']
 
 
 # Application definition
@@ -116,3 +116,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
